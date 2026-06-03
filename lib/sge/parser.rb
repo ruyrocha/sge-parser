4d4072a0ac30ab0176632b4ee0da46ebe4d5d477
Ruy Rocha <108208+ruyrocha@users.noreply.github.com>
2026-06-03 15:19:50 -0300

    Initial project files.

---
+++ lib/sge/parser.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "parser/version"
+
+module Sge
+  module Parser
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sge/parser/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Sge
+  module Parser
+    VERSION = "0.1.0"
+  end
+end
